44b026db2b462a0f521e504eba2a4be68241e0ab
David Tsai <32302812+dtsai7@users.noreply.github.com>
2021-01-03 20:20:08 +0800

    CH2 Grid Approximation

---
+++ CH2Grid.R
@@ -0,0 +1,24 @@
+# Chapter II.
+## Grid Approximation
+# define grid
+p_grid <- seq(from=0, to=1, length.out= 20)
+
+# define prior
+prior <- rep(1, 20)
+
+# compute likelihood at each value in grid
+likelihood <- dbinom(6, size= 9, prob= p_grid)
+
+# compute product of likelihood and prior 
+unstd.posterior <- likelihood * prior
+
+# standardize the posterior(so it sums to 1)
+posterior <- unstd.posterior / sum(unstd.posterior)
+
+
+plot(p_grid, posterior, type="b",
+      xlab="probability of water", ylab="posterior probability")
+
+mtext("20 points")
+
+
